e2216b2ae62c535876197ef00dfabf2d1bef75b3
Jenny <tasicying@gmail.com>
2025-08-12 17:35:05 -0500

    update form to no database

--- index.js
+++ index.js
@@ -8,21 +8,10 @@ dotenv.config({ path: '.env' })
 const port = 3000;
 const app = express();
 
-const db = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PW,
-    port: process.env.PORT,
-    ssl: { require: true, rejectUnauthorized: false },
-    idleTimeoutMillis: 30000
-})
 
-db.connect();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.json());
 
 app.get("/", (req, res)=> {
     res.render("index.ejs");
@@ -37,15 +26,7 @@ app.get("/contacts", (req, res)=> {
 })
 
 app.post("/contacts", async(req, res)=>{
-    try{
-        const {name, email, textarea} = req.body
-        await db.query("INSERT INTO message (name, email, message) VALUES ($1, $2, $3)", [name, email, textarea])
-        res.render("partials/submit.ejs")
-    } catch(err) {
-        console.log(err);
-        res.redirect("/");
-    };
-
+    res.render("partials/submit.ejs")
 })
 
 app.listen(port, ()=>{
