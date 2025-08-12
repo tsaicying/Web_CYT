f567d2b0dd53b5b972735bda2852203079b6277f
Jenny <tasicying@gmail.com>
2025-08-12 16:39:54 -0500

    update database, submit page

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { Client } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config({ path: '.env' })
 
 const port = 3000;
 const app = express();
+
+const db = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PW,
+    port: process.env.PORT,
+    ssl: { require: true, rejectUnauthorized: false }
+})
+
+db.connect();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({entended:true}));
@@ -19,8 +34,15 @@ app.get("/contacts", (req, res)=> {
     res.render("partials/contacts.ejs");
 })
 
-app.post("/contacts", (req, res)=>{
-    console.log(req.body);
+app.post("/contacts", async(req, res)=>{
+    try{
+        const {name, email, textarea} = req.body
+        await db.query("INSERT INTO message (name, email, message) VALUES ($1, $2, $3)", [name, email, textarea])
+        res.render("partials/submit.ejs")
+    } catch(err) {
+        console.log(err)
+    };
+
 })
 
 app.listen(port, ()=>{
